cb449c5f2d4fccaee2387e58775f33b6a5681591
seung-hun-h <dau4723@gmail.com>
2023-01-28 15:07:46 +0900

    리팩토링

--- src/main/java/org/hamstudy/chap02/StringCalculator.java
+++ src/main/java/org/hamstudy/chap02/StringCalculator.java
@@ -4,40 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
-	private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile(
-		"//(\\D)\n(.+)"
-	);
-	private static final String DEFAULT_SEPARATORS = ",|;";
-	private static final int REGEX_SEPARATOR_IDX = 1;
-	private static final int REGEX_EXPRESSION_IDX = 2;
-
 	public int calculate(String input) {
-		if (input == null || input.isBlank()) {
-			throw new IllegalArgumentException(String.format("input should not be empty or blank. input: %s", input));
-		}
+		List<Integer> numbers = Expression.of(input)
+			.toNumbers();
 
-		String separator = DEFAULT_SEPARATORS;
-		String expression = input;
+		validatePositiveOrZero(numbers);
 
-		Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(input);
-		if (matcher.find()) {
-			separator = matcher.group(REGEX_SEPARATOR_IDX);
-			expression = matcher.group(REGEX_EXPRESSION_IDX);
-		}
-
-		return sum(toNumbers(separator, expression));
+		return sum(numbers);
 	}
 
 	private int sum(List<Integer> numbers) {
 		int sum = 0;
 		for (Integer number : numbers) {
-			validatePositiveOrZero(number);
 			sum = Calculator.add(sum, number);
 		}
 
 		return sum;
+	}
+
+	private void validatePositiveOrZero(List<Integer> numbers) {
+		for (Integer number : numbers) {
+			validatePositiveOrZero(number);
+		}
 	}
 
 	private void validatePositiveOrZero(Integer number) {
@@ -46,11 +37,31 @@ public class StringCalculator {
 		}
 	}
 
-	private List<Integer> toNumbers(String separator, String expression) {
-		return Arrays.stream(
-				expression.split(separator)
-			)
-			.map(Integer::parseInt)
-			.toList();
+	private record Expression(String separator, String value) {
+		private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile(
+			"//(\\D)\n(.+)"
+		);
+		private static final String DEFAULT_SEPARATORS = ",|;";
+		private static final int REGEX_SEPARATOR_IDX = 1;
+		private static final int REGEX_EXPRESSION_IDX = 2;
+
+		public static Expression of(String input) {
+			if (input == null || input.isBlank()) {
+				throw new IllegalArgumentException("input should not be empty or blank");
+			}
+
+			Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(input);
+			if (matcher.find()) {
+				return new Expression(matcher.group(REGEX_SEPARATOR_IDX), matcher.group(REGEX_EXPRESSION_IDX));
+			}
+
+			return new Expression(DEFAULT_SEPARATORS, input);
+		}
+
+		public List<Integer> toNumbers() {
+			return Arrays.stream(value.split(separator))
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
+		}
 	}
 }

15a28d3414951e661abb1f85c52ede0c9bb07af6
seunghun <dau4723@gmail.com>
2023-03-19 18:29:24 +0900

    jsp 파일 못찾는 에러 해결

--- src/main/java/next/web/ListUserServlet.java
+++ src/main/java/next/web/ListUserServlet.java
@@ -2,6 +2,9 @@ package next.web;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.db.DataBase;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -13,9 +16,11 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/user/list")
 public class ListUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ListUserServlet.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		LOGGER.info("request /user/list");
 		req.setAttribute("users", DataBase.findAll());
 		RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
 		rd.forward(req, resp);

267f2073696fdd62c807e91de330649921fee201
seunghun <dau4723@gmail.com>
2023-05-19 11:02:25 +0900

    step7 - 로그인한 사용자만 질문

--- src/main/java/next/controller/qna/CreateQuestionController.java
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -7,8 +7,10 @@ import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import next.controller.UserSessionUtils;
 import next.dao.QuestionDao;
 import next.model.Question;
+import next.model.User;
 
 public class CreateQuestionController extends AbstractController {
 	private static final Logger logger = LoggerFactory.getLogger(CreateQuestionController.class);
@@ -16,17 +18,23 @@ public class CreateQuestionController extends AbstractController {
 
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String writer = request.getParameter("writer");
+		if (!UserSessionUtils.isLogined(request.getSession())) {
+			return jspView("redirect:/users/loginForm");
+		}
+
+		User user = UserSessionUtils.getUserFromSession(request.getSession());
+
 		String title = request.getParameter("title");
 		String contents = request.getParameter("contents");
 
-		Question question = new Question(writer, title, contents);
+		Question question = new Question(user.getName(), title, contents);
 
 		questionDao.insert(question);
 
 		logger.info("Question is created! {}", question);
 
 
-		return jspView("/").addObject("questions", questionDao.findAll());
+		return jspView("redirect:/")
+			.addObject("questions", questionDao.findAll());
 	}
 }

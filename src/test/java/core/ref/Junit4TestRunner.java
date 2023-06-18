aca4438877df78d36ba75e5b76f8c0ee88fa5918
seunghun <dau4723@gmail.com>
2023-06-18 19:54:39 +0900

    step8 - 10.1.2.3 @MyTest 메서드 실행

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,21 @@
 package core.ref;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
-public class Junit4TestRunner {
+class Junit4TestRunner {
     @Test
-    public void run() throws Exception {
+    void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.getAnnotation(MyTest.class) != null)
+            .forEach(method -> {
+                try {
+                    method.invoke(clazz.getDeclaredConstructor().newInstance());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
     }
 }
